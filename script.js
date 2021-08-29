44183af49b5e04ac8ab084ec551880cf687d048b
cyberck <dushaev2017@gmail.com>
2021-08-29 10:37:52 -0700

    stepen

---
+++ script.js
@@ -0,0 +1,38 @@
+// Циклы
+
+
+let sifra = +prompt('Vvedite sifru');
+let stepen = +prompt('Vdeti stepen');
+
+alert(`${sifra ** stepen}`)
+
+
+
+
+
+
+
+
+// let num = +prompt('Vvedite sifru');
+// let num1 = +prompt('Vvedite sifru');
+
+// for(let i = 0; ) {
+
+// }
+// let admin, name;
+// name = 'Djon';         
+
+// admin = name;
+
+
+// alert(admin);
+
+
+// let age = prompt('Skolko tebe let', 100);
+
+// alert(`Tebe ${age} let`);
+
+// let name = prompt('Kak vas zovut');
+
+// alert(`${name}`);
+
